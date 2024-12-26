883cc578938c11fd64ae83a70ed9c35adaf9c155
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-12-26 21:52:59 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { navItems } from "@/data";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 
-export default function Home() {
+const Home = () {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
@@ -22,3 +22,4 @@ export default function Home() {
     </main>
   );
 }
+export default Home;
