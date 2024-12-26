311b5b7d3e224ed43de2bd82b0c6681ecf05b563
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-12-26 14:29:47 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "./provider";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
 
