50a8d1a7149d28fee85389604677e35a2602ffd0
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-12-26 21:10:09 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <Suspense fallback={<>Loading...</>}>
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
         <Experience />
         <Footer />
-      <Suspense fallback={<>Loading...</>}>
-        <MyComponent />
+      
         </Suspense>
       </div>
     </main>
