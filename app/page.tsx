cc7134592673ecbbd2dd1d4cd60ee9e3711cc362
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-12-26 21:57:00 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { navItems } from "@/data";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 
-const Home = () {
+const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
