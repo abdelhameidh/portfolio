0c1da653a3c619eee6295d2039f5c4dabbd4a06b
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-12-26 14:35:58 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,6 @@
 import { FaLocationArrow } from "react-icons/fa6";
 import Image from "next/image";
 
-import { socialMedia } from "@/data";
 import MagicButton from "./ui/MagicButton";
 
 const Footer = () => {
