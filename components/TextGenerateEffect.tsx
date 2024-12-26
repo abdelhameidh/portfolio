4b2fbd5defbcc0e3f915d009b2517925c453bf78
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-12-26 14:47:34 +0200

    Update TextGenerateEffect.tsx

--- components/TextGenerateEffect.tsx
+++ components/TextGenerateEffect.tsx
@@ -4,27 +4,23 @@ import { motion, stagger, useAnimate } from "framer-motion";
 import { cn } from '@/components/lib/utils';
 
 export const TextGenerateEffect = ({
-  words,
+ words,
   className,
-  filter = true,
-  duration = 0.5,
 }: {
   words: string;
   className?: string;
-  filter?: boolean;
-  duration?: number;
 }) => {
   const [scope, animate] = useAnimate();
   let wordsArray = words.split(" ");
   useEffect(() => {
+    console.log(wordsArray);
     animate(
       "span",
       {
         opacity: 1,
-        filter: filter ? "blur(0px)" : "none",
       },
       {
-        duration: duration ? duration : 1,
+        duration: 2,
         delay: stagger(0.2),
       }
     );
