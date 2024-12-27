fd5285a7254e0ce829e636ba815a71e3af257b7b
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-12-27 15:12:48 +0200

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -48,7 +48,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Currently building an AI based websites",
+      title: "Currently building an AI based websites and developing an autonomous driving vehicle simulation project.",
       description: "The Inside Scoop",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
