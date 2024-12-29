119292d5cc650821e1ab025db4d569ebcd1a364c
abdelhameidh <132206702+abdelhameidh@users.noreply.github.com>
2024-12-29 14:01:09 +0200

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -17,7 +17,7 @@ const RecentProjects = () => {
                 key={item.id}
               >
                 <PinContainer
-                  title={item.title}
+                  title={item.link}
                   href={item.link}
                 >
                   <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
